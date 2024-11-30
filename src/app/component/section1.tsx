ebe6746bd8d3daae374dd47f218373f81df3a003
sanoober <toobasadhayo454003@gmail.com>
2024-12-01 01:38:21 +0500

    Completed Assignment-6

--- src/app/component/section1.tsx
+++ src/app/component/section1.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import Image from "next/image";
 const Section1 = () => {
     return(
-    <section className="w-[1280px] h-[1028px] font-roboto">
-           <div className="flex justify-between md:flex-row flex-col items-center w-[1280px]">
-            <div className="w-[640px] h-[300px] xl:pr-[60px] pl-[10px] md:w-1/2 flex flex-col gap-[24px] xl:my-[250px] xl:pl-[80px] lg:my-[170px] md:my-[14px]">
+    <section className="w-[1280px] h-[1028px] top-[141px] ">
+        <main className="flex justify-between md:flex-row flex-col items-center w-[1280px] h-[900px] ">
+          
+            <div className="w-[640px] h-[300px] xl:pr-[60px] pl-[10px] md:w-1/2 flex flex-col gap-[24px] xl:my-[250px] xl:pl-[80px] lg:my-[170px] md:my-[14px] flex flex-row">
             <div className=" flex flex-col w-[500px] h-[212px]  gap-[24px]">
                 <h1 className="text-[56px] font-bold leading-[67.2px] md:text-[48px]">Learn new skills online with ease</h1>
                 <p className="text-[18px] md:text-[18px] leading-[27px] h-[54px]">Discover a wide range of courses covering a variety of subjects, taught by expert instructors.
@@ -15,12 +16,12 @@ const Section1 = () => {
                 <button className="w-[164px] h-[48px] rounded-[5px] border-solid border-black border-[1px]">Explore Courses</button>
             </div>
             </div>
-            <div className="w-full h-[650px] relative   overflow-hidden">
-            <Image src={'/pic.png'} alt="picture" layout="fill" objectFit="cover" className="rounded-md"  />
+            <div className="w-[640px] h-[750px] ">
+            <Image src={'/pic.png'} alt="picture" width={640} height={900} objectFit="cover" className="  rounded-md "  />
             </div>
-           </div>
-           
-           <div className="bg-[#f7f7f7] h-[228px]  gap-[64px] justify-between w-[1280px] flex py-8 mt-8">
+          
+           </main>
+           <div className="bg-[#f7f7f7] h-[228px]  gap-[64px] justify-between w-[1280px] flex">
             <div className="w-[312px] h-[68px] py-[80px] ml-[8px]">
                 <h5 className="font-bold text-[24px] leading-[33.6px] ">Trusted by 2000+ companies worldwide.</h5>
             </div>
